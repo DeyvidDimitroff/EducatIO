e66fa725eb04d07ac59985be32adc29a556abb60
DeyvidDimitroff <deivid.dimitroff@yahoo.com>
2016-11-27 19:39:38 +0200

    added gender to registration TODO: fix console output

--- routes/users.js
+++ routes/users.js
@@ -22,6 +22,7 @@ router.post('/register', function(req, res){
 	var username = req.body.username;
 	var password = req.body.password;
 	var password2 = req.body.password2;
+	var gender = req.body.gender;
 
 	// Validation
 	req.checkBody('name', 'Name is required').notEmpty();
@@ -30,6 +31,7 @@ router.post('/register', function(req, res){
 	req.checkBody('username', 'Username is required').notEmpty();
 	req.checkBody('password', 'Password is required').notEmpty();
 	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+	req.checkBody('gender', 'Gender is required').notEmpty();
 
 	var errors = req.validationErrors();
 
@@ -42,7 +44,8 @@ router.post('/register', function(req, res){
 			name: name,
 			email: email,
 			username: username,
-			password: password
+			password: password,
+			gender: gender
 		});
 
 		User.createUser(newUser, function(err, user){
