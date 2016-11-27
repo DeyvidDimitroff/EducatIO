80be3e4bdc7c74c664fcdd3c61f1d5d1725adbab
DeyvidDimitroff <deivid.dimitroff@yahoo.com>
2016-11-27 17:12:06 +0200

    Fixed authorisation, added some documentation and users list

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 //Get Homepage
-router.get('/', ensureAuthenticated, function(req, res){
+router.get('/',  function(req, res){
     res.render('index');
 });
 
-function ensureAuthenticated(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
-    } else {
-        res.redirect('/users/login');
-    }
-}
 
 module.exports =router;
